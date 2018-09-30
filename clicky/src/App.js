54c7630398b5f652a1e94b79abe5fa7b56320a66
Brietta Smith <bri@Briettas-MacBook-Pro.local>
2018-09-29 23:43:46 -0400

    update3

--- clicky/src/App.js
+++ clicky/src/App.js
@@ -6,7 +6,7 @@ import cards from "./cards.json";
 import "./App.css";
 
 class App extends Component {
-  // Setting this.state.cards to the cards json array
+  // Setting this.state.cards
   state = {
     cards,
     score: 0,
@@ -43,11 +43,11 @@ class App extends Component {
       }
     });
   }
-  // Map over this.state.cards and render a cardCard component for each card object
+  // render card component for each card object
   render() {
     return (
       <Wrapper>
-        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Game</Header>
+        <Header score={this.state.score} highscore={this.state.highscore}>Clicky Baby</Header>
         {this.state.cards.map(card => (
           <Card
             clickCount={this.clickCount}
